cde82a44a28e951a01ba47f8eec6098e360a5cf4
RandyLDP <80972485+RandyLDP@users.noreply.github.com>
2021-05-13 14:42:12 +0200

    Add files via upload

---
+++ Objects.js
@@ -0,0 +1,23 @@
+let person= {
+    name: 'Randy',
+    age: 27,
+    evaluations: [7, 10, 9]
+};
+//console.log(person);
+/* naam || age 
+console.log(person.name);
+console.log(person.age);
+*/
+//person['name'] = 'Randy';
+console.log(person.evaluations);
+
+//Deel 2
+
+let colors=[ 'green', 'blue', 'red'];
+    let count = colors.push('yellow', 5 ,{greeting:'hi ik ben een object'});
+    /*console.log(colors);
+    console.log(colors.length);
+    console.log(['green']);*/
+    console.log(['red']);
+
+
